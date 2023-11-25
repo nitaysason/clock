0cce844b38e8324c54588ca210832200095ac0be
nitaysason <nitaysason@gmail.com>
2023-11-25 22:41:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# new clock
+
+a digital clock
---
+++ script.js
@@ -0,0 +1,26 @@
+
+function updateClock() {
+  currentTime = new Date();
+  hours = currentTime.getHours();
+  minutes = currentTime.getMinutes();
+  seconds = currentTime.getSeconds();
+
+  console.log(`Current time: ${hours}:${minutes}:${seconds}`);
+
+  totaltime = `${hours}:${minutes}:${seconds}`;
+  time.textContent = totaltime;
+}
+setInterval(updateClock, 1000);
+
+
+let i = 0
+
+function cng_color() {
+  i = Math.floor(Math.random() * 255)
+
+
+  mycolor = `rgb(${i * 20 % 255}, ${i * 30 % 255}, ${i * 30 % 255})`
+  time.style.color = mycolor
+
+}
+setInterval(cng_color, 300);
